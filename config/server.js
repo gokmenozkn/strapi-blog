f9d868107f612cc29dcd986195e344efa49ff8c9
gokmenozkn <kanjey159@gmail.com>
2020-07-06 15:39:21 +0300

    server.js

--- config/server.js
+++ config/server.js
@@ -1,19 +1,4 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-// });
-
-module.exports = {
-  host: 'localhost',
-  port: 1337,
-  cron: {
-    enabled: false,
-  },
-  admin: {
-    path: '/', // Note: The administration will be accessible from the root of the domain (ex: http//yourfrontend.com/)
-    serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
-    build: {
-      backend: 'https://strapi-blog1.herokuapp.com',
-    },
-  },
-};
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+});
